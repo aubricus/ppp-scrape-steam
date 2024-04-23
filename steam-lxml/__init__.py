6755d5ce8bbdc11a921d6ec083fc1212f7ab1f90
Aubrey Taylor <aubricus@gmail.com>
2024-04-22 21:38:01 -0700

    Add names, prices, and tags

--- steam-lxml/__init__.py
+++ steam-lxml/__init__.py
@@ -1,7 +1,17 @@
 import requests
 import lxml.html
 
-URL = "https://store.steampowered.com/explore/new/"
+urls = {
+    "explore_new": "https://store.steampowered.com/explore/new/",
+}
+
+xpaths = {
+    "new_releases": '//div[@id="tab_newreleases_content"]',
+    "new_releases__item_name": './/div[@class="tab_item_name"]/text()',
+    "new_releases__item_price": './/div[@class="discount_final_price"]/text()',
+    "new_releases__item_tag": './/div[@class="tab_item_top_tags"]',
+    "new_releases__platform": './/div[@class="tab_item_details"]',
+}
 
 
 def get_html(url):
@@ -13,8 +23,21 @@ def get_doc(html):
 
 
 def main():
-    doc = get_doc(get_html(URL))
-    print(doc)
+    doc = get_doc(get_html(urls["explore_new"]))
+    new_releases_el = doc.xpath(xpaths["new_releases"])[0]
+
+    names = new_releases_el.xpath(xpaths["new_releases__item_name"])
+    print(f"Names: {names[:3]}...\n")
+
+    prices = new_releases_el.xpath(xpaths["new_releases__item_price"])
+    print(f"Prices: {prices[:3]}...\n")
+
+    tag_els = new_releases_el.xpath(xpaths["new_releases__item_tag"])
+    print(f"Tag Els: {tag_els[:3]}...\n")
+
+    tags = [el.text_content() for el in tag_els]
+    tags = [tag.split(", ") for tag in tags]
+    print(f"Tags: {tags[:3]}...\n")
 
 
 if __name__ == "__main__":
