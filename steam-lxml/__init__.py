c30c817f6765d10c79293591164e94f71ee9fd53
Aubrey Taylor <aubricus@gmail.com>
2024-04-22 21:12:57 -0700

    Setup basic GET

--- steam-lxml/__init__.py
+++ steam-lxml/__init__.py
@@ -1,5 +1,21 @@
+import requests
+import lxml.html
+
+URL = "https://store.steampowered.com/explore/new/"
+
+
+def get_html(url):
+    return requests.get(url)
+
+
+def get_doc(html):
+    return lxml.html.fromstring(html.content)
+
+
 def main():
-    print("Hello World");
+    doc = get_doc(get_html(URL))
+    print(doc)
+
 
 if __name__ == "__main__":
     main()
